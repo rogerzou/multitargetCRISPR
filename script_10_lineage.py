48e3c19d22c5698113d558b7e644ff2ddd4056a1
Chalberts <alberto-24-1@hotmail.com>
2021-09-16 21:47:44 -0400

    Added NGS data analysis scripts. Three different analysis, according to how samples are demultiplexed and how mutations are detected.

--- script_10_lineage.py
+++ script_10_lineage.py
@@ -11,23 +11,10 @@ import src.ltr as ltr
 import sys
 import os
 
-""" Determine run paths based on operating system """
-if sys.platform == "linux" or sys.platform == "linux2":     # File paths (Ubuntu)
-    genome_savepath = "/mnt/c/Users/Roger/Desktop/"         # Directory to store indexed genome
-    hg38 = ['hg38', "/mnt/c/Users/Roger/bioinformatics/hg38_bowtie2/hg38.fa"]
-    hg19 = ['hg19', "/mnt/c/Users/Roger/bioinformatics/hg19_bowtie2/hg19.fa"]
-    mm10 = ['mm10', "/mnt/c/Users/Roger/bioinformatics/mm10_bowtie2/mm10.fa"]
-    dr11 = ['dr11', "/mnt/c/Users/Roger/bioinformatics/dr11_bowtie2/dr11.fa"]
-    datadir = "/mnt/z/rzou4/NGS_data/4_damage/"             # Directory for input and output data
-elif sys.platform == "darwin":                              # File paths (macOS)
-    genome_savepath = "/Users/rogerzou/Desktop/"            # Directory to store indexed genome
-    hg38 = ['hg38', "/Users/rogerzou/bioinformatics/hg38_bowtie2/hg38.fa"]
-    hg19 = ['hg19', "/Users/rogerzou/bioinformatics/hg19_bowtie2/hg19.fa"]
-    mm10 = ['mm10', "/Users/rogerzou/bioinformatics/mm10_bowtie2/mm10.fa"]
-    dr11 = ['dr11', "/Users/rogerzou/bioinformatics/dr11_bowtie2/dr11.fa"]
-    datadir = "/Volumes/Lab-Home/rzou4/NGS_data/4_damage/"  # Directory for input and output data
-else:
-    sys.exit()
+dr11 = ['dr11', "/mnt/d/DATABASES/dr11_bowtie2/dr11.fa"]
+datadir = "/mnt/d/DATA/MULTITARGET_GRNA_DESIGN/"             # Directory for input and output data
+genome_savepath = "/mnt/d/DATA/MULTITARGET_GRNA_DESIGN/"         # Directory to store indexed genome
+hg38 = ['hg38', "/mnt/d/DATABASES/HUMAN_REF_GENOME/GRCh38_noalt_as/GRCh38_noalt_as.fa"]
 
 """ Set analysis path """
 ana = datadir + "Alu_ana_10_lineage/"
@@ -38,15 +25,18 @@ ana_2 = ana + "2_primers/"
 os.makedirs(ana_2) if not os.path.exists(ana_2) else None
 ana_3 = ana + "3_ampngs/"
 os.makedirs(ana_3) if not os.path.exists(ana_3) else None
+ana_chip = datadir + "Alu_ana_1_putative/1_protosearch/"
+psearch_hg38 = ana_chip + "psearch_hg38"
+psearch_out = ana_1 + "psearch_hg38_"
 
-
-""" Set file paths """
-psearch_hg38_Alu = datadir + "Alu_ana_1_putative/1_protosearch/psearch_hg38_Alu"
+""" Set file paths
+psearch_hg38_Alu = datadir + "Alu_ana_1_putative/1_protosearch/psearch_hg38"
 psearch_dr11_DR1 = datadir + "Alu_ana_1_putative/1_protosearch/psearch_dr11_DR1"
 psearch_dr11_DR2 = datadir + "Alu_ana_1_putative/1_protosearch/psearch_dr11_DR2"
 psearch_dr11_DAN = datadir + "Alu_ana_1_putative/1_protosearch/psearch_dr11_DAN"
+"""
 lineageNGS_210830 = datadir + "210830_lineageNGS/"
-
+lineageNGS_210908 = datadir + "210908_lineageNGS/DATA_ANA/"
 
 # """ ############################################################################################ """
 # """ For a given number (or sets of numbers) of targets, do the following:
@@ -75,26 +65,54 @@ lineageNGS_210830 = datadir + "210830_lineageNGS/"
 # ltr.get_primers_nested(gen, outfile, dr11[0], genome_savepath, ct_values=[*range(10, 41)])
 # # Run wrapper function that align primer pairs to genome, parses SAM, obtain stats on best gRNAs
 # ltr.bowtie_parse_stats_wrapper(outfile, dr11[1])
-#
-# # Find PCR primers for target sites of all hg38 Alu mgRNAs that target between 10-40 sites
-# outfile = ana_1 + "lineage_hg38_Alu_10-40"
-# gen = msa.gen_putative(psearch_hg38_Alu + ".sam")
-# ltr.get_primers_nested(gen, outfile, hg38[0], genome_savepath, ct_values=[*range(10, 41)])
+"""
+# Find PCR primers for target sites of all hg38 Alu mgRNAs that target between 10-40 sites
+outfile = ana_1 + "lineage_hg38_Alu"
+gen = msa.gen_putative(psearch_hg38 + ".sam")
+ltr.get_primers_nested(gen, outfile, hg38[0], genome_savepath, ct_values=[10])
 # # Run wrapper function that align primer pairs to genome, parses SAM, obtain stats on best gRNAs
-# ltr.bowtie_parse_stats_wrapper(outfile, hg38[1])
-#
-#
-# """ ############################################################################################ """
-# dr11_DR1_1 = "TGGGCTTGACAAACCACCTGNGG"
-# dr11_DR1_2 = "GTCTGCTAAATGACGTCATGNGG"
-# inpath = ana_1 + "lineage_dr11_DR1_10-40"
-# ltr.get_nested_primers(inpath + "_inn_msa", inpath + "_out_msa", ana_2 + "dr11_DR1_1", dr11_DR1_1)
-# ltr.get_nested_primers(inpath + "_inn_msa", inpath + "_out_msa", ana_2 + "dr11_DR1_2", dr11_DR1_2)
-
-
-hg38_Alu_1 = "CCAGGCTGGAGTGCAGTGCT"
-gen = msa.gen_putative(psearch_hg38_Alu + ".sam", subset=[hg38_Alu_1])
-msa.get_target_sequences(gen, ana_3 + "psearch_hg38_Alu_1", hg38[0], genome_savepath, win=500)
-msa.parse_target_sequences(ana_3 + "psearch_hg38_Alu_1.csv", hg38_Alu_1, ana_3 + "hg38_Alu_1")
-
-ltr.lineage_ngs(lineageNGS_210830 + "EP-1-8", hg38[1])
+ltr.bowtie_parse_stats_wrapper(outfile, hg38[1])
+# ###########################################################################################
+# Get targets stats for the best 10-target-mgRNAs
+ct10S1 = "CCAGGCTGGAGTGCAGTGCT"
+ct10S2 = "AGCTACTCGGGAGGCCAAAG"
+ct10S3 = "TGCACTCCAGCCTGGATAAC"
+ct10S4 = "GGAGTGCAGTGGCAATATCT"
+ct10S5 = "TTTCACCGTGTTGGCCAGCA"
+ct10S6 = "AAGTGCTGGGATTATAGGCC"
+gen = msa.gen_putative(psearch_hg38 + ".sam", subset=[ct10S3])
+msa.get_targets_stats(gen, hg38[0], psearch_out + "ct10S3", chromhmm=True)
+gen = msa.gen_putative(psearch_hg38 + ".sam", subset=[ct10S4])
+msa.get_targets_stats(gen, hg38[0], psearch_out + "ct10S4", chromhmm=True)
+gen = msa.gen_putative(psearch_hg38 + ".sam", subset=[ct10S5])
+msa.get_targets_stats(gen, hg38[0], psearch_out + "ct10S5", chromhmm=True)
+gen = msa.gen_putative(psearch_hg38 + ".sam", subset=[ct10S6])
+msa.get_targets_stats(gen, hg38[0], psearch_out + "ct10S6", chromhmm=True) """
+# ###########################################################################################
+# Analyze mutation rates in NGS data using three different approaches.
+mut_rate_file1 = lineageNGS_210908 + "mutation_rates_reg_dose_1.csv"
+mut_rate_file2 = lineageNGS_210908 + "mutation_rates_reg_dose_2.csv"
+mut_rate_file3 = lineageNGS_210908 + "mutation_rates_reg_dose_3.csv"
+out_rate1 = open(mut_rate_file1, 'w')
+out_rate2 = open(mut_rate_file2, 'w')
+out_rate3 = open(mut_rate_file3, 'w')
+Time_Points = [0, 0.5, 1, 2, 3, 5]
+cter = 0
+out_rate1.write("Time, T1, T2, T3, T4, T5, T6, T7, T8\n")
+out_rate2.write("Time, T1, T2, T3, T4, T5, T6, T7, T8\n")
+out_rate3.write("Time, T1, T2, T3, T4, T5, T6, T7, T8\n")
+for file in "WT", "Reg-12h", "Reg-24h", "Reg-2d", "Reg-3d", "Reg-5d":
+    infile = lineageNGS_210908 + file
+    Mut1 = ltr.lineage_ngs(infile, hg38[1])
+    Mut2 = ltr.lineage_ngs2(infile)
+    Mut3 = ltr.lineage_ngs3(infile)
+    out_rate1.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (Time_Points[cter], Mut1[0], Mut1[1], Mut1[2], Mut1[3]
+                                                  , Mut1[4], Mut1[5], Mut1[6], Mut1[7]))
+    out_rate2.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (Time_Points[cter], Mut2[0], Mut2[1], Mut2[2], Mut2[3]
+                                                  , Mut2[4], Mut2[5], Mut2[6], Mut2[7]))
+    out_rate3.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (Time_Points[cter], Mut3[0], Mut3[1], Mut3[2], Mut3[3]
+                                                   , Mut3[4], Mut3[5], Mut3[6], Mut3[7]))
+    cter = cter + 1
+out_rate1.close()
+out_rate2.close()
+out_rate3.close()
--- src/ltr.py
+++ src/ltr.py
@@ -8,7 +8,7 @@ from Bio import SeqIO, SeqRecord
 
 
 def get_primers_nested(gen, outfile, genome_str, savepath, ct_values, npr=3,
-                       rad1=100, rad2=200, rad3=350):
+                       rad1=75, rad2=150, rad3=250):
     """
     :param gen: bowtie2 alignments for each putative protospacer sequence of format:
         - boolean: whether it is a new putative protospacer sequence
@@ -98,13 +98,18 @@ def get_primer3_primers(seq, num_primers, rng_min, rng_max, prod_size=None):
     return primers_lt, primers_rt
 
 
-def _get_primer3_primers_helper(inptprmr3, seq, num_primers, rng_min, rng_max, ps=None):
+def _get_primer3_primers_helper(inptprmr3, seq, num_primers, rng_min, rng_max, ps=None, mltplx=0):
     """ Write primer3 input file settings to "inptprmr3". Settings that can be changed are:
     :param num_primers:  # primer pairs that will be sought in each of the nested PCR
     :param rng_min: min range of sequence that is considered target region for PCR.
     :param rng_max: max range of sequence that is considered target region for PCR.
     :param ps: desired amplicon size.
+    :param mltplx: if multiplexed PCR is needed. Sets more stringent conditions on primers.
     """
+    if mltplx == 1:
+        min_gc, max_gc, min_tm, max_tm = 40, 60, 58, 60
+    else:
+        min_gc, max_gc, min_tm, max_tm = 35, 65, 57, 63
     # Write sequence target (primers will bind outside of this)
     with open(inptprmr3, 'w') as prim3file:
         prim3file.write("%s%s\n" % ("SEQUENCE_ID=Proto-", 0))
@@ -120,10 +125,15 @@ def _get_primer3_primers_helper(inptprmr3, seq, num_primers, rng_min, rng_max, p
         prim3file.write("PRIMER_OPT_SIZE=20\n")
         prim3file.write("PRIMER_MIN_SIZE=18\n")
         prim3file.write("PRIMER_MAX_SIZE=22\n")
-        prim3file.write("PRIMER_MAX_GC=65\n")
-        prim3file.write("PRIMER_MIN_GC=35\n")
-        prim3file.write("PRIMER_MIN_TM=57\n")
-        prim3file.write("PRIMER_MAX_TM=63\n")
+        prim3_maxGC = "PRIMER_MAX_GC=" + str(max_gc) + "\n"
+        prim3file.write(prim3_maxGC)
+        prim3_minGC = "PRIMER_MIN_GC=" + str(min_gc) + "\n"
+        prim3file.write(prim3_minGC)
+        prim3_maxtm = "PRIMER_MAX_TM=" + str(max_tm) + "\n"
+        prim3file.write(prim3_maxtm)
+        prim3_mintm = "PRIMER_MIN_TM=" + str(min_tm) + "\n"
+        prim3file.write(prim3_mintm)
+        prim3file.write("PRIMER_MAX_POLY_X=4\n") if mltplx == 1 else None
         prim3file.write("PRIMER_PRODUCT_SIZE_RANGE=100-1000\n") if ps is None else prim3file.write(
             "PRIMER_PRODUCT_SIZE_RANGE=%s\n" % ps)
         prim3file.write("PRIMER_EXPLAIN_FLAG=1\n")
@@ -250,11 +260,27 @@ def get_nested_primers(f_inn, f_out, outfile, protospacer):
     np.savetxt(outfile + '_inn_%s.csv' % protospacer, np.asarray(p_inn), fmt='%s', delimiter=',')
     np.savetxt(outfile + '_out_%s.csv' % protospacer, np.asarray(p_out), fmt='%s', delimiter=',')
 
-
-def lineage_ngs(ngsfile, genome_path):
+def lineage_ngs(ngsfile, genome_path, verbose = 1):
     """
-    Assuming read1 is 250bp, read2 is 50bp.
+    Mutation analysis script #1.
+        1) Truncates paired-end fastq files and aligns to the genome using bowtie in paired-end mode.
+        2) Checks for alignments that are in proximity to the msa of the mgRNA targets.
+        3) Then checks for the presence of the protospacer in the read. If present, the
+            read is considered intact. Else, it's considered mutated.
+    Parameters of the script:
+        ngsfile: name of the input fastq.
+        genome_path: path to genome in pickle format.
+        verbose: displays progress in reading the sam file.
     """
+    # Define the msa of the targets: chromosome + position:
+    chr_tgt = ["chr16", "chr16", "chr6", "chr1", "chr19", "chr16", "chr11", "chr7"]
+    pos_tgt = [11642579, 70421720, 86513528, 68153228, 1904845, 72558952, 93681278, 37733626]
+    proto = "CCAGGCTGGAGTGCAGTGCT"
+    win = 2000 # window to tell whether an alignment falls within a target region
+    num_tgt = 10
+    num_align = [0]*num_tgt
+    num_intact = [0]*num_tgt
+    ratio_mutated = [0]*num_tgt
     reads1 = SeqIO.parse(open(ngsfile + "_1.fastq"), 'fastq')
     reads2 = SeqIO.parse(open(ngsfile + "_2.fastq"), 'fastq')
     out1, out2 = [], []
@@ -270,6 +296,112 @@ def lineage_ngs(ngsfile, genome_path):
         out2.append(ngs_2)
     SeqIO.write(out1, ngsfile + "_1_trunc.fq", "fastq")
     SeqIO.write(out2, ngsfile + "_2_trunc.fq", "fastq")
-
-    sp.run(['bowtie2', '-p', '8', '--local', '--no-discordant', '-x', genome_path[:-3],
+    sp.run(['bowtie2', '-p', '8', '--local', '--very-sensitive', '--no-discordant', '-x', genome_path[:-3],
             '-1', ngsfile + '_1_trunc.fq', '-2', ngsfile + '_2_trunc.fq', '-S', ngsfile + '.sam'])
+    with open(ngsfile + '.sam', 'r') as sam_it:
+        cter = 0
+        for read in sam_it:     # read every line of SAM file
+            # counter to track progress
+            if verbose and cter % 10000 == 0:
+                print("lineage_ngs(): Read %i lines of SAM file." % cter)
+            cter += 1
+            # skip SAM header lines
+            if read[0] == '@':
+                continue
+            row = read.strip().split('\t')
+            tgt = 0
+            for chr, pos in zip(chr_tgt, pos_tgt):
+                if ((row[2] == chr) and (int(row[3])-win < pos < int(row[3])+win)):
+                    num_align[tgt] += 1
+                    sequence = row[0].strip().split('_')
+                    if sequence[1].find(proto) > 0:
+                        num_intact[tgt] += 1
+                tgt += 1
+    for i in range(len(num_align)):
+        ratio_mutated[i] = 1-num_intact[i]/num_align[i] if (num_align[i] > 0) else 0
+    #return num_align, ratio_mutated
+    return ratio_mutated
+
+def qual_filt(infile, outfile, qual=30, perc=95):
+    # Performs quality filter using the command "fastq_quality_filter" from FASTX-Toolkit
+    filtered_file = outfile + "_1.fastq"
+    inputfile = infile + "_1.fastq"
+    sp.run(['fastq_quality_filter', '-q', str(qual), '-p', str(perc), '-i', inputfile, '-Q', '33', '-o', filtered_file])
+
+def lineage_ngs2(ngsfile):
+    """
+    Mutation analysis script #2.
+        1) Checks first bases and demultiplexes according to the hamming distance to the second round PCR primers.
+        2) Then checks for the presence of the protospacer in the read. If present, the
+            read is considered intact. Else, it's considered mutated.
+    Parameters of the script:
+        ngsfile: name of the input fastq.
+    """
+    proto = "CCAGGCTGGAGTGCAGTGCT"
+    threshold = 2
+    # 2nd round PCR primers for each of the targets are defined for demultiplexing.
+    prmrs_tgts = {0: "TGGAGGTGTCATTTGCCCAG", 1: "ACAGCTTGGGGTTACACAGG",
+                  2: "TGTTCAAGCAAAAGGTTCAGCT", 3: "TCCTCGGGGTTCTACCTCTC",
+                  4: "CTGGGCCAAACAAGCACATT", 5: "GCTGCCTTGCCAGAGTTTTT",
+                  6: "TCAGCTGTTTGATCTCAGGCA", 7: "GGGTCTAGCAAGTGGGCAAT",
+                  8: "CCAACGTTGTTCAGGCACAC", 9: "CTCCTTGGCCCTTGGTTCAT"}
+    num_tgt = 10
+    num_align = [0]*num_tgt
+    num_intact = [0]*num_tgt
+    ratio_mutated = [0]*num_tgt
+    # Perform quality filter.
+    ngs_filtered = ngsfile + "_filt"
+    qual_filt(ngsfile, ngs_filtered, qual=30, perc=95)
+    # Go through filtered file to demultiplex and find the mutations in protospacer.
+    reads1 = SeqIO.parse(open(ngs_filtered + "_1.fastq"), 'fastq')
+    for ngs_1 in reads1:
+        for i in prmrs_tgts:
+            check_primer = prmrs_tgts[i]
+            if hamming_distance(ngs_1[0:len(check_primer)], check_primer) < threshold:
+                num_align[i] = num_align[i] + 1
+                if ngs_1.seq.find(proto) > 1:
+                    num_intact[i] = num_intact[i] + 1
+    for i in range(len(num_align)):
+        ratio_mutated[i] = 1-num_intact[i]/num_align[i] if (num_align[i] > 0) else 0
+    return ratio_mutated
+
+def lineage_ngs3(ngsfile, threshold = 2):
+    """
+    Mutation analysis script #3.
+        1) Checks first bases and demultiplexes according to the hamming distance to the second round PCR primers.
+        2) Checks for "key" sequences: sets of 8 bases that are adjacent to the protospacer on either side. If the
+            distance between them is different than 28 (20 for proto + 8 for key), then an indel is called.
+    Parameters of the script:
+        ngsfile: name of the input fastq.
+    """
+    prmrs_tgts = {0: "TGGAGGTGTCATTTGCCCAG", 1: "ACAGCTTGGGGTTACACAGG",
+                  2: "TGTTCAAGCAAAAGGTTCAGCT", 3: "TCCTCGGGGTTCTACCTCTC",
+                  4: "CTGGGCCAAACAAGCACATT", 5: "GCTGCCTTGCCAGAGTTTTT",
+                  6: "TCAGCTGTTTGATCTCAGGCA", 7: "GGGTCTAGCAAGTGGGCAAT",
+                  8: "CCAACGTTGTTCAGGCACAC", 9: "CTCCTTGGCCCTTGGTTCAT"}
+    key1 = {0: "TCTGTTGG", 1: "ACTCCAGC", 2: "TGTGTTAT", 3: "TCTGTTGT", 4: "TCTGTCAC",
+            5: "TCCGTCAC", 6: "TCTGTCGC", 7:"TCTGTCAC", 8: "TCTGTTGC", 9: "TCTGTTGC"}
+    key2 = {0: "GGGATCTT", 1: "AGGATCAT", 2: "TGGCTTAC", 3: "GGGATCTC", 4: "AGGCTCAC",
+            5: "GGGATCTT", 6: "GGGATCTT", 7: "GGGATCTC", 8: "CGGCTCAC", 9: "GGGGGATC"}
+    num_tgt = 10
+    num_align = [0] * num_tgt
+    num_intact = [0] * num_tgt
+    ratio_mutated = [0] * num_tgt
+    reads_merged = SeqIO.parse(open(ngsfile + "_1.fastq"), 'fastq')
+    for ngs_1 in reads_merged:
+        for i in prmrs_tgts:
+            check_primer = prmrs_tgts[i]
+            if hamming_distance(ngs_1[0:len(check_primer)], check_primer) < threshold:
+                if ((ngs_1.seq.find(key1[i]) > 0) and (ngs_1.seq.find(key2[i]) > 0)):
+                    num_align[i] = num_align[i] + 1
+                    if (ngs_1.seq.find(key2[i])-ngs_1.seq.find(key1[i])) == 28:
+                        num_intact[i] = num_intact[i] + 1
+    for i in range(len(num_align)):
+        ratio_mutated[i] = 1-num_intact[i]/num_align[i] if (num_align[i] > 0) else 0
+    return ratio_mutated
+
+def hamming_distance(string1, string2):
+    if len(string1) != len(string2):
+        raise ValueError('Strings should be the same length')
+    else:
+        return sum(c1 != c2 for c1, c2 in zip(string1, string2))
